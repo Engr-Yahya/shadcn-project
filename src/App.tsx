068b3b50baa497d05413a3f7b3b01d2eb15c8804
Yahya <yahyashafiq309@gmail.com>
2026-04-06 12:22:32 +0500

    feat: add form component with validation using react-hook-form
    - Created a new Form component that utilizes react-hook-form for managing form state and validation.
    - Implemented input fields for first name and last name with validation rules.
    - Added error messages for validation feedback.
    
    feat: implement Card component for UI structure
    
    - Introduced a Card component to encapsulate content with styling and layout.
    - Added CardHeader, CardTitle, CardDescription, CardAction, CardContent, and CardFooter subcomponents for flexible usage.
    
    feat: create Input component for standardized input fields
    
    - Developed an Input component with customizable styles and properties for consistent input handling across the application.
    
    feat: add Label component for form labeling
    
    - Created a Label component to enhance accessibility and styling for form labels.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import { Button } from "@/components/ui/button"
-
+import Form from "./components/Form"
+import "./index.css"
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    <div className="mt-20 flex justify-center items-center h-full">
+      <Form />
     </div>
+
+    // <div className="flex min-h-svh p-6">
+    //   <div className="flex w-full max-w-full min-w-0 flex-col items-center justify-center gap-4 text-sm leading-loose">
+    //     <Card className="px-8 py-8 rounded-xl">
+    //       <h1 className="font-medium">Project ready!</h1>
+    //       <p>You may now add components and start building.</p>
+    //       <p>We&apos;ve already added the button component for you.</p>
+    //       <div className="flex gap-2">
+    //         <Button className="">Button</Button>
+    //         <Button variant="destructive" className="">Delete</Button>
+    //       </div>
+    //     </Card>
+    //     <div className="font-mono text-xs text-muted-foreground">
+    //       (Press <kbd>d</kbd> to toggle dark mode)
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
---
+++ src/components/Form.tsx
@@ -0,0 +1,64 @@
+import { useForm } from "react-hook-form"
+import { Button } from "./ui/button"
+import { Card } from "./ui/card"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+interface FormData {
+  firstName: string
+  middleName: string
+  lastName: string
+}
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  function onSubmit(data: FormData) {
+    console.log("user Input", data)
+  }
+
+  return (
+    <Card className="p-8">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Label className="mb-2">FirstName: </Label>
+          <Input
+            className="bg-white text-black"
+            {...register("firstName", {
+              required: true,
+              minLength: {
+                value: 3,
+                message: "Name length must be at least 3",
+              },
+            })}
+          />
+          {errors.firstName && <p> {errors.firstName.message} </p>}
+        </div>
+        <br />
+        <div>
+          <Label className="mb-2">LastName: </Label>
+          <Input
+            className="bg-white text-black"
+            {...register("lastName", {
+              required: true,
+              minLength: {
+                value: 3,
+                message: "Name length must be at least 3",
+              },
+            })}
+          />
+          {errors.lastName && <p> {errors.lastName.message} </p>}
+        </div>
+        <Button type="submit" className="mx-auto mt-4 w-fit px-6 py-2">
+          Submit
+        </Button>
+      </form>
+    </Card>
+  )
+}
+
+export default Form
